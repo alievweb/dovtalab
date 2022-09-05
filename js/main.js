022f839cd5681d97b58cff8692ae47e90bdad80c
P A R V I Z <107940120+alievweb@users.noreply.github.com>
2022-09-05 09:17:13 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,395 @@
+// Кластери 1- Забони точики  15 Савол
+
+function orfo() {
+	$("#par16").append("Орфоэпия").css("color","red");
+	$("#savoli1").hide();
+	$("#savoli2").show(100);
+}
+
+function leksiko() {
+	$("#par16").append("Лексикография").css("color","red");
+	$("#savoli1").hide();
+	$("#savoli2").show(100);
+}
+
+function fonetika() {
+	$("#par1").append("Фонетика").css("color","green");
+	$("#savoli1").hide();
+	$("#savoli2").show(100);
+}
+
+function fraza() {
+	$("#par16").append("Фразеология").css("color","red");
+	$("#savoli1").hide();
+	$("#savoli2").show(100);
+}
+
+// Саволи 2
+
+function jang() {
+	$("#par17").append("ҷангу ҷидол").css("color","red");
+	$("#savoli2").hide();
+	$("#savoli3").show(100);
+}
+
+function garm() {
+	$("#par2").append("гарму сард").css("color","green");
+	$("#savoli2").hide();
+	$("#savoli3").show(100);
+}
+
+function libos() {
+	$("#par17").append("сару либос").css("color","red");
+	$("#savoli2").hide();
+	$("#savoli3").show(100);
+}
+
+function sharm() {
+	$("#par17").append("шарму ҳаё").css("color","red");
+	$("#savoli2").hide();
+	$("#savoli3").show(100);
+}
+
+// Саволи 3
+
+function leksiya() {
+	$("#par18").append("лексия").css("color","red");
+	$("#savoli3").hide();
+	$("#savoli4").show(100);
+}
+
+function dotsent() {
+	$("#par3").append("доцент").css("color","green");
+	$("#savoli3").hide();
+	$("#savoli4").show(100);
+}
+
+function konsert() {
+	$("#par18").append("консерт").css("color","red");
+	$("#savoli3").hide();
+	$("#savoli4").show(100);
+}
+
+function yosuman() {
+	$("#par18").append("ёсуман").css("color","red");
+	$("#savoli3").hide();
+	$("#savoli4").show(100);
+}
+
+// Саволи 4
+
+function bonu() {
+	$("#par19").append("бону, дору, орзу").css("color","red");
+	$("#savoli4").hide();
+	$("#savoli5").show(100);
+}
+
+function umr() {
+	$("#par19").append("умр, усто, устод").css("color","red");
+	$("#savoli4").hide();
+	$("#savoli5").show(100);
+}
+
+function bud() {
+	$("#par19").append("буд, шуд, дуд").css("color","red");
+	$("#savoli4").hide();
+	$("#savoli5").show(100);
+}
+
+function kush() {
+	$("#par4").append("куш, мур, хурд").css("color","green");
+	$("#savoli4").hide();
+	$("#savoli5").show(100);
+}
+
+// Саволи 5
+
+function zur() {
+	$("#par20").append("зӯр, кӯр, шӯр").css("color","red");
+	$("#savoli5").hide();
+	$("#savoli6").show(100);
+}
+
+function sur() {
+	$("#par5").append("сӯр, хӯр, хӯрд").css("color","green");
+	$("#savoli5").hide();
+	$("#savoli6").show(100);
+}
+
+function buy() {
+	$("#par20").append("бӯй, гӯй, кӯй").css("color","red");
+	$("#savoli5").hide();
+	$("#savoli6").show(100);
+}
+
+function tanur() {
+	$("#par20").append("танӯр, танбӯр, ҷӯр").css("color","red");
+	$("#savoli5").hide();
+	$("#savoli6").show(100);
+}
+
+// Саволи 6
+
+function mura() {
+	$("#par21").append("танӯр, танбӯр, ҷӯр").css("color","red");
+	$("#savoli6").hide();
+	$("#savoli7").show(100);
+}
+
+function mur() {
+	$("#par21").append("мӯра, мӯтарам, сӯбат").css("color","red");
+	$("#savoli6").hide();
+	$("#savoli7").show(100);
+}
+
+function muhra() {
+	$("#par21").append("мӯҳра, мӯҳтарам, сӯҳбат").css("color","red");
+	$("#savoli6").hide();
+	$("#savoli7").show(100);
+}
+
+function muhr() {
+	$("#par6").append("муҳра, муҳтарам, суҳбат").css("color","green");
+	$("#savoli6").hide();
+	$("#savoli7").show(100);
+}
+
+// Саволи 7
+
+function ha() {
+	$("#par22").append("ҳа, бале, оре").css("color","red");
+	$("#savoli7").hide();
+	$("#savoli8").show(100);
+}
+
+function koshki() {
+	$("#par22").append("кошки, илоҳи, мабодо").css("color","red");
+	$("#savoli7").hide();
+	$("#savoli8").show(100);
+}
+
+function mi() {
+	$("#par7").append("-мӣ, -чӣ, оё").css("color","green");
+	$("#savoli7").hide();
+	$("#savoli8").show(100);
+}
+
+function kani() {
+	$("#par22").append("канӣ, фақат, танҳо").css("color","red");
+	$("#savoli7").hide();
+	$("#savoli8").show(100);
+}
+
+// Саволи 8
+
+function payvast() {
+	$("#par23").append("пайваст ва тобеъ").css("color","red");
+	$("#savoli8").hide();
+	$("#savoli9").show(100);
+}
+
+function asli() {
+	$("#par8").append("аслӣ ва нисбӣ").css("color","green");
+	$("#savoli8").hide();
+	$("#savoli9").show(100);
+}
+
+function mikdor() {
+	$("#par23").append("миқдорӣ ва тартибӣ").css("color","red");
+	$("#savoli8").hide();
+	$("#savoli9").show(100);
+}
+
+function khos() {
+	$("#par23").append("хос ва ҷинс").css("color","red");
+	$("#savoli8").hide();
+	$("#savoli9").show(100);
+}
+
+// Саволи 9
+
+function har() {
+	$("#par24").append("ҳар кас, ҳар кадом").css("color","red");
+	$("#savoli9").hide();
+	$("#savoli10").show(100);
+}
+
+function man() {
+	$("#par9").append("ман, ту, вай").css("color","green");
+	$("#savoli9").hide();
+	$("#savoli10").show(100);
+}
+
+function hamon() {
+	$("#par24").append("ин, он, ҳамон").css("color","red");
+	$("#savoli9").hide();
+	$("#savoli10").show(100);
+}
+
+function hej() {
+	$("#par24").append("ҳеҷ кас, ҳеҷ кадом").css("color","red");
+	$("#savoli9").hide();
+	$("#savoli10").show(100);
+}
+
+// Саволи 10
+
+function agar() {
+	$("#par25").append("агар, ар, гар").css("color","red");
+	$("#savoli10").hide();
+	$("#savoli11").show(100);
+}
+
+function va() {
+	$("#par25").append("ва, ӯ, ю").css("color","red");
+	$("#savoli10").hide();
+	$("#savoli11").show(100);
+}
+
+function zero() {
+	$("#par25").append("зеро, чунки, ки").css("color","red");
+	$("#savoli10").hide();
+	$("#savoli11").show(100);
+}
+
+function vakte() {
+	$("#par10").append("вақте ки, ҳамин ки, чун").css("color","green");
+	$("#savoli10").hide();
+	$("#savoli11").show(100);
+}
+
+// Саволи 11
+
+function malum() {
+	$("#par26").append("маълум").css("color","red");
+	$("#savoli11").hide();
+	$("#savoli12").show(100);
+}
+
+function anana() {
+	$("#par26").append("маълум").css("color","red");
+	$("#savoli11").hide();
+	$("#savoli12").show(100);
+}
+
+function tab() {
+	$("#par11").append("табъ").css("color","green");
+	$("#savoli11").hide();
+	$("#savoli12").show(100);
+}
+
+function naf() {
+	$("#par26").append("нафъ").css("color","red");
+	$("#savoli11").hide();
+	$("#savoli12").show(100);
+}
+
+// Саволи 12
+
+function khazon() {
+	$("#par27").append("xазон").css("color","red");
+	$("#savoli12").hide();
+	$("#savoli13").show(100);
+}
+
+function mardumon() {
+	$("#par12").append("мардумон").css("color","green");
+	$("#savoli12").hide();
+	$("#savoli13").show(100);
+}
+
+function bokhirad() {
+	$("#par27").append("бохирад").css("color","red");
+	$("#savoli12").hide();
+	$("#savoli13").show(100);
+}
+
+function bekhirad() {
+	$("#par27").append("бехирад").css("color","red");
+	$("#savoli12").hide();
+	$("#savoli13").show(100);
+}
+
+// Саволи 13
+
+function ord() {
+	$("#par28").append("орд").css("color","red");
+	$("#savoli13").hide();
+	$("#savoli14").show(100);
+}
+
+function tojik() {
+	$("#par13").append("тоҷик").css("color","green");
+	$("#savoli13").hide();
+	$("#savoli14").show(100);
+}
+
+function donishgo() {
+	$("#par28").append("донишгоҳ").css("color","red");
+	$("#savoli13").hide();
+	$("#savoli14").show(100);
+}
+
+function sang() {
+	$("#par28").append("санг").css("color","red");
+	$("#savoli13").hide();
+	$("#savoli14").show(100);
+}
+
+// Саволи 14
+
+function nomi() {
+	$("#par29").append("номи ашёро").css("color","red");
+	$("#savoli14").hide();
+	$("#savoli15").show(100);
+}
+
+function harakat() {
+	$("#par29").append("ҳаракати ашёро").css("color","red");
+	$("#savoli14").hide();
+	$("#savoli15").show(100);
+}
+
+function ashyo() {
+	$("#par14").append("чигунагии ашёро").css("color","green");
+	$("#savoli14").hide();
+	$("#savoli15").show(100);
+}
+
+function amalu() {
+	$("#par29").append("амалу ҳолат").css("color","red");
+	$("#savoli14").hide();
+	$("#savoli15").show(100);
+}
+
+// Саволи 15
+
+function bozor() {
+	$("body").css("background","whitesmoke");
+	$("#par30").append("бозор").css("color","red");
+	$("#savoli15").hide();
+	$("#savoli16").show(100);
+}
+
+function raft() {
+	$("body").css("background","whitesmoke");
+	$("#par30").append("рафт").css("color","red");
+	$("#savoli15").hide();
+	$("#savoli16").show(100);
+}
+
+function sharif() {
+	$("body").css("background","whitesmoke");
+	$("#par15").append("Шариф").css("color","green");
+	$("#savoli15").hide();
+	$("#savoli16").show(100);
+}
+
+function ba() {
+	$("body").css("background","whitesmoke");
+	$("#par30").append("ба").css("color","red");
+	$("#savoli15").hide();
+	$("#savoli16").show(100);
+}
+
+// Натиҷаи тест
